339c338b3ce7fadadc0dc2186ce4b9e8a867d25a
Joanna Szopa <joanna.aksamit5@wp.pl>
2022-04-07 19:32:26 +0200

    add

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@
             booksId: 'data-id',
             bookImage: '.book__image',
             bookName: '.book__name',
-            bookFilters: '.filters',
+            bookFilters: '.filters input',
 
         }
 
@@ -18,16 +18,38 @@
     const templates = {
         productBook: Handlebars.compile(document.querySelector(select.templateOf.productBook).innerHTML),
     };
+
     function render() {
         for (let book of dataSource.books) {
+           
             const generatedHTML = templates.productBook(book);
             const element = utils.createDOMFromHTML(generatedHTML);
             const bookList = document.querySelector(select.books.bookList);
             bookList.appendChild(element);
+            const ratingBgc = determineRatingBgc(book.rating);
+            book.rating = ratingBgc;
+            const ratingWidth = book.rating * 10;
+            book.ratingWidth = ratingWidth;
+
+
+            
         }
     }
     render();
+    function determineRatingBgc(rating){
+        let background = '';
+      if(rating < 5){
+        background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+      }else if(rating > 5 && rating <= 7){
+        background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+      }else if(rating > 7 && rating <= 9){
+        background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+      }else if(rating > 9){
+        background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+      }
 
+       return background; 
+    }
     function initActions(){
 
         const favoriteBooks = [];
@@ -55,12 +77,69 @@
               });  
             }
             console.log('favoriteBooks');
-        }
         
+        
+        
+        const filtersBooks = document.querySelector(select.books.bookFilters);
+
+        filtersBooks.addEventListener('click',function(callback){
+         const bookElem = callback.target;
+          const bookValue = callback.target.value;{
+         
+          if(bookElem.checked){
+            const filters = [];
+            filters.push(bookValue);
+            console.log(filters);
+          }else {
+              const filters = [];
+           const indexValue = filters.indexOf(bookElem) ;
+           filters.splice(indexValue,1);  
+           console.log(filters)
+          }
+         
+          
+          }
+         
+
+         
+            
+
+         
+         filterBooks();
+
+           
+        })
+        function filterBooks(){
+             for (let book of dataSource.books) {
+                const filteredBook = document.querySelector('.book__image[data-id="' + book.id + '"]');    
+                let shouldBeHidden = false;
+                const filterElem = document.querySelectorAll(select.books.bookFilters);    
+            for (let filter of filterElem){
+              console.log(filter)
+                if (!book.details[filter]){
+                    shouldBeHidden = true;
+                    break;
+                }
+                 
+                }
+                if(shouldBeHidden){
+                    
+                filteredBook.classList.add('hidden');
+                }else{
+                    
+                  filteredBook.classList.remove('hidden');
+               }
+            }
+           
+            }
+
+        }
+    }
+      
         initActions();
 
 
-}
+
 
 
         
